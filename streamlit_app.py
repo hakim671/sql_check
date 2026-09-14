0f96a6a5ed8e7230673ad012dbf925587f711c41
hakim671 <boyzodahakim2008@gmail.com>
2026-09-14 15:32:39 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import psycopg2
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("My PostgreSQL App")
+
+try:
+    conn = psycopg2.connect(
+        host=st.secrets["DB_HOST"],
+        port=st.secrets["DB_PORT"],
+        database=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"]
+    )
+
+    st.success("✅ PostgreSQL connected!")
+
+    df = pd.read_sql("SELECT * FROM users", conn)
+
+    st.dataframe(df)
+
+    conn.close()
+
+except Exception as e:
+    st.error(f"❌ Connection error: {e}")
